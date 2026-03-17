c5ad1a86564d5c770fba34b36adb7e2cb4089e91
sisoin <sokk159dps@gmail.com>
2026-03-17 14:26:03 +0900

    Qdrant 설정 추가 및 Settings 보완
    - qdrant_url, qdrant_collection 필드 추가
    - extra = "ignore" 로 미정의 환경변수 허용

--- battery_market_agent/config/settings.py
+++ battery_market_agent/config/settings.py
@@ -14,9 +14,14 @@ class Settings(BaseSettings):
     chunk_overlap: int = 200
     retriever_k: int = 5
 
+    # Qdrant
+    qdrant_url: str = "http://localhost:6333"
+    qdrant_collection: str = "battery_docs"
+
     # 데이터
     data_dir: str = "./data"
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "ignore"
